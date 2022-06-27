f0565faa6c5eb47c4ffdc95a2e4de2c418650c21
Zion Gonet <ziongonet@gmail.com>
2022-06-27 02:17:11 +0100

    added api key and made requests

--- weather.py
+++ weather.py
@@ -1,1 +1,20 @@
+from wsgiref.util import request_uri
 import requests
+import math
+
+API_KEY = '70e89b01d957b9430680d0a43110fda6'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+city = input('Enter a city name: ')
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = data['main']['temp'] - 273
+
+    
+else: 
+    print("There was an error!")
